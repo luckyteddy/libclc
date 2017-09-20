248411a63a755806ca4420cbe629a92bb85df502
Jan Vesely <jan.vesely@rutgers.edu>
2017-09-20 18:02:53 -0400

    Add initial support for half precision builtins
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- generic/include/clc/float/definitions.h
+++ generic/include/clc/float/definitions.h
@@ -80,6 +80,11 @@
 #define HALF_MIN_10_EXP -4
 #define HALF_MIN_EXP    -13
 
+#define HALF_RADIX 	2
+#define HALF_MAX 	0x1.ffcp15h
+#define HALF_MIN 	0x1.0p-14h
+#define HALF_EPSILON 	0x1.0p-10h
+
 #endif
 
 #endif
--- generic/include/math/clc_ldexp.h
+++ generic/include/math/clc_ldexp.h
@@ -4,3 +4,7 @@ _CLC_DEF _CLC_OVERLOAD float __clc_ldexp(float, int);
   #pragma OPENCL EXTENSION cl_khr_fp64 : enable
   _CLC_DEF _CLC_OVERLOAD double __clc_ldexp(double, int);
 #endif
+#ifdef cl_khr_fp16
+  #pragma OPENCL EXTENSION cl_khr_fp16 : enable
+  _CLC_DEF _CLC_OVERLOAD float __clc_ldexp(half, int);
+#endif
